92f72d18564256e53dce3f3f6c95a624335e657c
Melissa2016 <32329679+Melissa2016@users.noreply.github.com>
2018-05-08 21:42:34 +0200

    user commit

--- src/main/java/se/backend/groupred2/model/User.java
+++ src/main/java/se/backend/groupred2/model/User.java
@@ -1,13 +1,49 @@
-package se.backend.groupred2.model;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-
-@Entity
-public class User {
-
-    @GeneratedValue
-    private Long id;
-
-
-}
+//package se.backend.groupred2.model;
+//
+//import com.fasterxml.jackson.annotation.JsonCreator;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//public class User {
+//
+//
+//    @Id
+//    @GeneratedValue
+//
+//    private Long id;
+//    @Column(nullable = false)
+//    private String firstName;
+//    @Column(nullable = false)
+//    private String lastName;
+//    private String status;
+//    //@ManyToOne
+//    //private Team team;
+//
+//    protected User(){}
+//
+//    @JsonCreator
+//    public User( Long id ,String firstName, String lastName, String status ) {
+//        this.id = id;
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.status = status;
+//
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public String getStatus() {
+//        return status;
+//    }
+//}
