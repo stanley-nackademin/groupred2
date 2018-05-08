bf9b06897920e544e00f9d835bf7dae14ea7e8ae
Richard <richardhags@gmail.com>
2018-05-08 13:22:10 +0200

    first commit

---
+++ src/main/java/se/backend/groupred2/Groupred2Application.java
@@ -0,0 +1,12 @@
+package se.backend.groupred2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Groupred2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Groupred2Application.class, args);
+	}
+}
---
+++ src/main/java/se/backend/groupred2/model/User.java
@@ -0,0 +1,13 @@
+package se.backend.groupred2.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
+@Entity
+public class User {
+
+    @GeneratedValue
+    private Long id;
+
+
+}
