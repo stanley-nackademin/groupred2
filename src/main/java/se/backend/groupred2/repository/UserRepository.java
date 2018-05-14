3befbcdded40e1862364b5525e619dfb48df365f
Daven Mäenpää <daven.maenpaa@outlook.com>
2018-05-14 20:18:10 +0200

    countAll till count

--- src/main/java/se/backend/groupred2/repository/UserRepository.java
+++ src/main/java/se/backend/groupred2/repository/UserRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-    int countAllByTeam(Team team);
+    int countByTeam(Team team);
 
     List<User> findByUserNumber(Long userNumber);
 
