a67bdee4be8f90309667c4d0c1c2c461c3d158c9
Daven <daven.maenpaa@outlook.com>
2018-05-15 11:32:58 +0200

    mapper

--- src/main/java/se/backend/groupred2/resource/mapper/InvalidUserMapper.java
+++ src/main/java/se/backend/groupred2/resource/mapper/InvalidUserMapper.java
@@ -9,11 +9,12 @@ import javax.ws.rs.ext.Provider;
 import static java.util.Collections.singletonMap;
 import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
 
+
 @Provider
 public final class InvalidUserMapper implements ExceptionMapper<InvalidUserException> {
 
     @Override
     public Response toResponse(InvalidUserException exception) {
-        return Response.status(BAD_REQUEST).entity(singletonMap("Error!", exception.getMessage())).build();
+        return Response.status(BAD_REQUEST).entity(singletonMap("error", exception.getMessage())).build();
     }
 }
