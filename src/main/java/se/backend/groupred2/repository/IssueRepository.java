4f5939529c89bda6db9af3b7687d51a97b427082
marcuskarlberg <marcus.karlberg@hotmail.com>
2018-08-27 17:05:24 +0200

    added test for createTask

--- src/main/java/se/backend/groupred2/repository/IssueRepository.java
+++ src/main/java/se/backend/groupred2/repository/IssueRepository.java
@@ -2,11 +2,13 @@ package se.backend.groupred2.repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import se.backend.groupred2.model.Issue;
 import se.backend.groupred2.model.Task;
 
 import java.util.List;
 
+@Repository
 public interface IssueRepository extends CrudRepository<Issue, Long> {
 
     @Query("SELECT DISTINCT i.task FROM Issue i")
---
+++ src/test/java/se/backend/groupred2/service/TaskServiceTest.java
@@ -0,0 +1,49 @@
+package se.backend.groupred2.service;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import se.backend.groupred2.model.Task;
+import se.backend.groupred2.model.TaskStatus;
+import se.backend.groupred2.repository.TaskRepository;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskServiceTest {
+
+    @Autowired
+    TaskService taskService;
+
+    @Autowired
+    TaskRepository taskRepository;
+
+    Task task;
+
+    @Before
+    public void setUp() {
+        task = new Task("TaskTest", "test the createTask method", TaskStatus.UNSTARTED);
+    }
+
+    @Test
+    public void createTaskTest() throws Exception {
+        taskService.createTask(task);
+        task.setId(1L);
+
+        Optional<Task> result = taskRepository.findById(task.getId());
+        Task taskFromDatabase = result.get();
+
+        assertEquals(task.toString(), taskFromDatabase.toString());
+    }
+
+    @After
+    public void tearDown(){
+        taskRepository.delete(task);
+    }
+}
