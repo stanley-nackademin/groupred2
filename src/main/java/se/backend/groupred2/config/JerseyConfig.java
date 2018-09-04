24d542edc9c3021f92b56e433a9bc8ba2bb1b2a7
stanley <stanley.ho@yh.nackademin.se>
2018-09-04 18:37:21 +0200

    Made changes to how JerseyConfig register classes

--- src/main/java/se/backend/groupred2/config/JerseyConfig.java
+++ src/main/java/se/backend/groupred2/config/JerseyConfig.java
@@ -5,12 +5,19 @@ import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import se.backend.groupred2.resource.IssueResource;
+import se.backend.groupred2.resource.TaskResource;
+import se.backend.groupred2.resource.TeamResource;
+import se.backend.groupred2.resource.UserResource;
 
 @Configuration
 public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
-        packages("se.backend.groupred2.resource");
+        register(IssueResource.class);
+        register(TaskResource.class);
+        register(TeamResource.class);
+        register(UserResource.class);
     }
 
     @Bean
